2f9823c857bbfce4a02f93d89c2f75d5769dfec2
neet-007 <moayedeffect@gmail.com>
2024-12-12 17:28:10 +0300

    now the line numbers are aligned with the text and the text i map out is aligned with the text arte

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 let text = "";
 
+const VALID_INPUT = [
+    "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H",
+    "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P",
+    "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X",
+    "y", "Y", "z", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+];
+
 const lineNumbers = document.createElement("div");
 lineNumbers.classList.add("line-numbers");
 const firstLine = document.createElement("div");
@@ -9,30 +16,56 @@ lineNumbers.appendChild(firstLine);
 document.body.appendChild(lineNumbers);
 
 const textArea = document.createElement("textarea");
-textArea.addEventListener("keydown", newLine);
-const textDiv = document.createElement("div");
+textArea.addEventListener("keydown", handleInput);
+textArea.addEventListener("input", handleTextAreaDebuh);
 textArea.classList.add("text-area");
 textArea.spellcheck = false;
 textArea.autocomplete = "off";
 
+const textAreaDebug = document.createElement("textarea");
+textAreaDebug.classList.add("text-area-debug");
+textAreaDebug.spellcheck = false;
+textAreaDebug.autocomplete = "off";
+
+const textDiv = document.createElement("div");
 textDiv.classList.add("text-div");
 const firstCol = document.createElement("div");
 firstCol.classList.add("text-div-col");
 textDiv.appendChild(firstCol);
+
 document.body.append(textArea);
+document.body.append(textAreaDebug);
 document.body.append(textDiv);
+
+function handleTextAreaDebuh(e) {
+    textAreaDebug.value = e.target.value;
+}
+
+/**
+ * @param {string | undefined} word=false
+ */
+
+function newLine(word = undefined) {
+    const col = document.createElement("div");
+    col.classList.add("text-div-col");
+    if (word !== undefined) {
+        const wordDiv = document.createElement("span");
+        wordDiv.innerHTML = word;
+        col.appendChild(wordDiv);
+    }
+
+    textDiv.appendChild(col);
+    const lineNumber = document.createElement("div");
+    lineNumber.innerHTML = `${lineNumbers.children.length + 1}`;
+    lineNumbers.appendChild(lineNumber);
+}
 
 /**
  * @param {KeyboardEvent} e - The input event from the textarea.
  */
-function newLine(e) {
+function handleInput(e) {
     if (e.key === "Enter") {
-        const col = document.createElement("div");
-        col.classList.add("text-div-col");
-        textDiv.appendChild(col);
-        const lineNumber = document.createElement("div");
-        lineNumber.innerHTML = `${lineNumbers.children.length + 1}`;
-        lineNumbers.appendChild(lineNumber);
+        newLine();
         return
     }
 
@@ -59,13 +92,12 @@ function newLine(e) {
         return
     }
 
+    if (!VALID_INPUT.includes(e.key)) {
+        return;
+    }
+
     if (last === undefined) {
-        const col = document.createElement("div");
-        col.classList.add("text-div-col");
-        const word = document.createElement("span");
-        word.innerHTML = e.key;
-        col.appendChild(word);
-        textDiv.appendChild(col);
+        newLine(e.key);
         return
     }
 
@@ -82,10 +114,7 @@ function newLine(e) {
     }
 
     last.children[last.children.length - 1].innerHTML += e.key;
-    if (last.getBoundingClientRect().width >= 500) {
-        console.log("here");
-        const col = document.createElement("div");
-        col.classList.add("text-div-col");
-        textDiv.appendChild(col);
+    if (last.getBoundingClientRect().width >= 500 - 8) {
+        newLine();
     }
 }
